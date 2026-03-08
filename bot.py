dad61802336cb71f7aa356096cda64368caf6c26
adabarbulescu <adabarbu2000@yahoo.com>
2026-03-08 12:28:22 +0200

    Initial bot setup

---
+++ bot.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot connected as {bot.user}")
+
+bot.run(TOKEN)
